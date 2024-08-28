b58efa764cea3ab226549d446443ce50e3da4917
aditi-bodkhe <a.bodkhe@elsevier.com>
2024-08-28 14:15:27 +0530

    CreatedGroup
    CreatedGroup

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class creategroup(models.Model):
+    title=models.CharField(max_length=100,blank=False)
+    description=models.TextField(max_length=300,blank=False)
+    image=models.ImageField(upload_to='', blank=True)
+    
+
+    def __str__(self):
+        return self.title
--- groups/views.py
+++ groups/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import creategroup
 
 def groups(request):
-    return render (request,'groups/groups.html')
+    if request.method == 'POST':
+        name=request.POST['name']
+        description=request.POST['description']
+        image=request.POST['image']
+
+        creategroupdata=creategroup(title=name,description=description,image=image)
+        creategroupdata.save()
+    result = creategroup.objects.all()
+    dicti = { 'result' : result }
+    return render(request,'groups/groups.html',dicti)
+
+    
